f04c109cbbcc1f8533d7768eb348099c8be14cdf
DudaaMoura <mariamourapalmital@gmail.com>
2023-10-25 17:09:33 +0000

    dfhfcgnfgn

---
+++ Aluno.java
@@ -0,0 +1,71 @@
+public class Aluno{
+    private String nome;
+    private double media;
+    private int numFaltas;
+    private int numAulas;
+
+    public Aluno(){
+
+    }
+
+    public String getNome(String nome){
+        return nome;
+
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getMedia() {
+        return media;
+    }
+    public void setMedia(double media) {
+        this.media = media;
+    }
+    public int getNumAulas() {
+        return numAulas;
+    }
+    public void setNumAulas(int numAulas) {
+        this.numAulas = numAulas;
+    }
+    public int getNumFaltas() {
+        return numFaltas;
+    }
+    public void setNumFaltas(int numFaltas) {
+        this.numFaltas = numFaltas;
+    }
+
+    public Aluno(String nome){
+        this.nome = nome;
+    }
+    public Aluno(String nome, double media){
+        this.nome = nome;
+        this.media = media;
+    }
+    public Aluno(String nome, double media, int numFaltas){
+        this.nome = nome;
+        this.media = media;
+        this.numFaltas = numFaltas;
+    }
+    public Aluno(String nome, double media, int numFaltas, int numAulas){
+        this.nome = nome;
+        this.media = media;
+        this.numFaltas = numFaltas;
+        this.numAulas = numAulas;
+    }
+
+    public void cadastrar(String nome){
+        this.nome = nome;
+    }
+
+    public boolean verificaSituacao(){
+        if((media<5) ||(numFaltas>25)){
+        return false;
+    }
+        else if ((media>=5) && (numFaltas<25)){
+    return true;
+}
+else
+return true;
+}}
